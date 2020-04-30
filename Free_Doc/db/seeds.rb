0a890c9180b27edd52dd5a63f081e7698c97262d
Homarv <pierratmarvin3@gmail.com>
2020-04-30 18:36:48 +0200

    Version2

--- Free_Doc/db/seeds.rb
+++ Free_Doc/db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+    doctor=Doctor.create(first_name:Faker::JapaneseMedia::DragonBall.character, last_name:Faker::Name.last_name,
+    speciality:["généraliste", "Chirurgien", "dentiste"].sample, zip_code: "#{rand(1000..9000)}0")
+end 
+
+10.times do 
+    patient= Patient.create(first_name:Faker::JapaneseMedia::OnePiece.character,last_name:Faker::Name.last_name ) 
+end 
+
+20.times do 
+rdv= Appointement.create(date:Faker::Date.in_date_period(year: 2020, month: 5) , doctor: Doctor.all.sample, patient: Patient.all.sample)
+end
