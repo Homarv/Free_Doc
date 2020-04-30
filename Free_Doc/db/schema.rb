dd9a7e2bd5e8e8a724a0e23cf673147c0ca2561b
Homarv <pierratmarvin3@gmail.com>
2020-04-30 21:50:17 +0200

    Version3

--- Free_Doc/db/schema.rb
+++ Free_Doc/db/schema.rb
@@ -10,16 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_30_141944) do
+ActiveRecord::Schema.define(version: 2020_04_30_165101) do
 
   create_table "appointements", force: :cascade do |t|
     t.datetime "date"
     t.integer "doctor_id"
     t.integer "patient_id"
+    t.integer "city_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["city_id"], name: "index_appointements_on_city_id"
     t.index ["doctor_id"], name: "index_appointements_on_doctor_id"
     t.index ["patient_id"], name: "index_appointements_on_patient_id"
+  end
+
+  create_table "cities", force: :cascade do |t|
+    t.string "name_city"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "doctors", force: :cascade do |t|
@@ -27,15 +35,19 @@ ActiveRecord::Schema.define(version: 2020_04_30_141944) do
     t.string "last_name"
     t.string "speciality"
     t.string "zip_code"
+    t.integer "city_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["city_id"], name: "index_doctors_on_city_id"
   end
 
   create_table "patients", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
+    t.integer "city_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["city_id"], name: "index_patients_on_city_id"
   end
 
 end
--- Free_Doc/db/seeds.rb
+++ Free_Doc/db/seeds.rb
@@ -7,15 +7,20 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
 
+
+10.times do
+    city= City.create(name_city:Faker::Address.city)
+end
+
 10.times do
     doctor=Doctor.create(first_name:Faker::JapaneseMedia::DragonBall.character, last_name:Faker::Name.last_name,
-    speciality:["généraliste", "Chirurgien", "dentiste"].sample, zip_code: "#{rand(1000..9000)}0")
+    speciality:["généraliste", "Chirurgien", "dentiste"].sample, zip_code: "#{rand(1000..9000)}0",city:City.all.sample)
 end 
 
 10.times do 
-    patient= Patient.create(first_name:Faker::JapaneseMedia::OnePiece.character,last_name:Faker::Name.last_name ) 
+    patient= Patient.create(first_name:Faker::JapaneseMedia::OnePiece.character,last_name:Faker::Name.last_name,city:City.all.sample ) 
 end 
 
 20.times do 
-rdv= Appointement.create(date:Faker::Date.in_date_period(year: 2020, month: 5) , doctor: Doctor.all.sample, patient: Patient.all.sample)
+    rdv= Appointement.create(date:Faker::Date.in_date_period(year: 2020, month: 5) , doctor: Doctor.all.sample, patient: Patient.all.sample,city:City.all.samp)
 end
